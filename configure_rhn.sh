3cbdf0dc7928793e84d44348dd040bc21281a421
jbride <jbride2001@yahoo.com>
2016-08-10 08:38:09 -0600

    docs for configure_rhn.sh

--- configure_rhn.sh
+++ configure_rhn.sh
@@ -1,3 +1,14 @@
+# Purpose
+#  Before installation of the MBaaS, you must first register each node in the cluster with the Red Hat Subscription Manager (RHSM).
+#  The registration enables OSE to access the Docker container images of RHMAP components hosted in the Red Hat Docker Registry.
+#  This script automates this registration across your OSE3 cluster
+#
+# Usage
+    ,/configure_rhn.sh <RHN User Id> <RHN password> <poolId of subscription with RHMAP entitlements>
+
+# Example
+#   ./configure_rhn.sh rhn-gps-jab XXXXX  8a85f9843e3d687a013e3ddd471a083e
+
 for node in infranode1.example.com  \
         node1.example.com \
         node2.example.com \
