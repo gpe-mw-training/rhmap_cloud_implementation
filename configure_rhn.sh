3387317fbf3fe9784cd35b665928d8717f33bf5c
jbride <jbride2001@yahoo.com>
2016-08-13 10:14:55 -0600

    https://github.com/redhat-gpe/rhmap_cloud_implementation/issues/6

--- configure_rhn.sh
+++ configure_rhn.sh
@@ -11,6 +11,9 @@
 # Example
 #   ./configure_rhn.sh rhn-gps-jab XXXXX  8a85f9843e3d687a013e3ddd471a083e
 
+export MONGODB_IMAGE=rhmap41/mongodb
+export MONGODB_IMAGE_VERSION=3.2-1
+
 for node in infranode1.example.com  \
         node1.example.com \
         node2.example.com \
@@ -18,5 +21,5 @@ for node in infranode1.example.com  \
         node4.example.com; 
 do 
         echo -en "\nConfiguring RHN for: $node\n"; 
-        ssh $node " subscription-manager register --username=$1 --password=$2; subscription-manager attach --pool=$3; mv /etc/yum.repos.d/open.repo /etc/yum.repos.d/open.repo.bk; yum install -y subscription-manager-plugin-container --disablerepo=* --enablerepo=rhel-7-server-optional-rpms; /usr/libexec/rhsmcertd-worker"; 
+        ssh $node " subscription-manager register --username=$1 --password=$2; subscription-manager attach --pool=$3; mv /etc/yum.repos.d/open.repo /etc/yum.repos.d/open.repo.bk; yum install -y subscription-manager-plugin-container --disablerepo=* --enablerepo=rhel-7-server-optional-rpms; /usr/libexec/rhsmcertd-worker; docker pull $MONGODB_IMAGE:$MONGODB_IMAGE_VERSION"; 
 done
