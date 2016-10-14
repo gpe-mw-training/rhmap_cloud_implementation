e285524f4cbfa84065001e5c7142f953c811503c
jbride <jbride2001@yahoo.com>
2016-10-14 09:56:37 -0600

    update to configure_rhn.sh

--- configure_rhn.sh
+++ configure_rhn.sh
@@ -18,9 +18,9 @@ for node in infranode1.example.com  \
         node1.example.com \
         node2.example.com \
         node3.example.com \
-        node4.example.com \ 
-        node5.example.com \ 
-        node6.example.com; 
+        node4.example.com \
+        node5.example.com \
+        node6.example.com;
 do 
         echo -en "\nConfiguring RHN for: $node\n"; 
         ssh $node " subscription-manager register --username=$1 --password=$2; subscription-manager attach --pool=$3; mv /etc/yum.repos.d/open.repo /etc/yum.repos.d/open.repo.bk; yum install -y subscription-manager-plugin-container --disablerepo=* --enablerepo=rhel-7-server-optional-rpms; /usr/libexec/rhsmcertd-worker; docker pull $MONGODB_IMAGE:$MONGODB_IMAGE_VERSION"; 
